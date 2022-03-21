815e77a0413b62a29c8768fad5fe1f4253d65ea4
Sandlayth <modepadu95@riseup.net>
2022-03-21 20:10:42 +0100

    Create operation parser

--- models/operation.go
+++ models/operation.go
@@ -1,13 +1,40 @@
-package model
+package models
 
 import (
     "time"
+    "errors"
+    "strings"
+    "strconv"
 )
+
+var IdCounter = 0
 
 type Operation struct {
     Id            int       `json: id`
     Date          time.Time `json: "date"`
     EffectiveDate time.Time `json: "effectiveDate"`
     Label         string    `json: "id"`
-    Balance       float     `json: "balance"`
+    Amount        float64   `json: "amount"`
+}
+
+func (h Operation) parseOperation(s string) (*Operation, error) {
+    parsedString := strings.Split(s, ";")[:]
+    if len(parsedString) < 6 {
+        return nil, errors.New("Failed to parse line")
+    }
+    IdCounter += 1
+    date, err := time.Parse("02/01/2006", parsedString[0])
+    effectiveDate, err := time.Parse("02/01/06", strings.Fields(parsedString[2])[1])
+    label := strings.SplitAfterN(parsedString[2], " ", 3)[2]
+    amount, err := strconv.ParseFloat(strings.Replace(parsedString[6], ",", ".", 1), 2)
+
+    o := Operation {
+        Id:            IdCounter,
+        Date:          date,
+        EffectiveDate: effectiveDate,
+        Label:         label,
+        Amount:        amount,
+    }
+
+    return &o, err
 }
