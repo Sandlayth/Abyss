dc7aee31ef2c41b1bc25295055b24b2097b4c7b2
Sandlayth <modepadu95@riseup.net>
2022-03-26 16:17:53 +0100

    Create Gorm connections

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+    "github.com/sandlayth/abyss/models"
     "github.com/gin-gonic/gin"
 )
 
 func main() {
     router := gin.Default()
+    models.ConnectDatabase()
     router.Run()
 }
